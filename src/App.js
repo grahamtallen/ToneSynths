02b40806d5bee1a18cd080145b003351a074e798
Graham Allen <grahamtallen94@gmail.com>
2017-12-19 13:57:18 -0800

    live editing

--- src/App.js
+++ src/App.js
@@ -1,73 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
 import Tone from 'tone';
-import controls from './Controls'
-import pannerCreator from './Panner'
-import {toJS} from 'mobx';
-import {handleMouseMove} from './mouseEvents'
-// Arpeggiator
-// var synth = new Tone.Synth().toMaster();
-//
-// var pattern = new Tone.Pattern(function(time, note){
-//     synth.triggerAttackRelease(note, 0.25);
-// }, ["C4", "E4", "G4", "A4", "C5", "D4", "G4"]).start(0);
+
+var panner = new Tone.AutoPanner({
+    "frequency": 4,
+    "depth": 1
+}).toMaster().start();
+//AutoFilter - a filter modulation effect
+var filter = new Tone.AutoFilter({
+    "frequency": 2,
+    "depth": 0.6
+}).toMaster().start();
+//Tremolo - an amplitude modulation effect
+var tremolo = new Tone.Tremolo({
+    "frequency": 4,
+    "depth": 0.4
+}).toMaster().start();
+//the input oscillators
+var osc0 = new Tone.Oscillator({
+    "volume": -Infinity,
+    "type": "square6",
+    "frequency": "C2"
+}).connect(panner).start();
 
 
-/**
- *  PIANO
- */
-
-
-var piano = new Tone.PolySynth(5, Tone.Synth, {
-    "volume" : -8,
-    "oscillator" : {
-        "partials" : [1, 2, 1],
-    },
-    "portamento" : 0.05
-}).toMaster()
-var cChord = ["C4", "E4", "G4", "B4"];
-var dChord = ["D4", "F4", "A4", "C5"];
-var gChord = ["B3", "D4", "E4", "A4"];
-var pianoPart = new Tone.Part(function(time, chord){
-
-    var filter = new Tone.AutoFilter({
-        frequency: toJS(controls.filterFrequency),
-        depth: toJS(controls.filterDepth)
-    }).toMaster().start();
-
-    // let panner = pannerCreator({
-    //     frequency: toJS(controls.panFrequency),
-    //     depth: toJS(controls.panDepth)
-    // })
-    piano.triggerAttackRelease(chord, "9n", time).connect(filter);
-}, [["0:0:2", cChord], ["0:1", cChord], ["0:1:3", dChord], ["0:2:2", cChord], ["0:3", cChord], ["0:3:2", gChord]])
-    .start("0");
-pianoPart.loop = true;
-pianoPart.loopEnd = "1m";
-pianoPart.humanize = false;
-Tone.Transport.bpm.value = 80;
-
+var osc1 = new Tone.Oscillator({
+    "volume": -Infinity,
+    "type": "square6",
+    "frequency": "G2"
+}).connect(panner).start();
 
 class App extends Component {
+
+  componentDidMount() {
+
+  }
+
 
 
   render() {
     return (
-      <div className="App" onMouseMove={(e) => handleMouseMove(e)}>
-        <header className="App-header">
-            <button onClick={() => Tone.Transport.stop()}  className="App-logo" alt="logo">
-                Stop
-            </button>
-            <button onClick={() => Tone.Transport.start("+0.1")} className="App-title">
-                Start
-            </button>
-        </header>
-        <button onClick={() => {
-            controls.arpLength = controls.arpLength + 0.1;
-        }} className="App-intro">
-          Longer
-        </button>
-          <p id="demo"></p>
+      <div className="App" id="DragContainer" >
+
+          <button onClick={() => osc0.volume.rampTo(0, 0.1)}>Start1</button>
+          <button onClick={() => osc0.volume.rampTo(-Infinity, 0.0)}>Start1</button>
+          <button onClick={() => osc1.volume.rampTo(0, 0.1)}>Start2</button>
+          <button onClick={() => osc1.volume.rampTo(-Infinity, 0.0)}>Start2</button>
+          <br />
+          <button onClick={() => {
+              panner.wet.value = 0.1;
+//fade to 100% wet over 3 seconds.
+              panner.wet.rampTo(1, 3);
+              // panner.set('frequency', 4)
+          }}>Tone set</button>
       </div>
     );
   }
